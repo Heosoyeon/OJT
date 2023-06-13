88882bec41c43356a46002139b46bd1623c9774d
DT051680 <DT051680@mobis.co.kr>
2023-06-13 18:01:59 +0900

    add OJT_4

---
+++ OJT_4/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Operator {
+private:
+    int num1;
+    int num2;
+    int result;
+    
+protected:
+    void setResult(int result) { this->result = result; }
+    int getNum1() { return num1; }
+    int getNum2() { return num2; }
+    virtual calculate() = 0;
+
+public:
+    void setNumber(int num1, int num2) { this->num1 = num1; this->num2 = num2; }
+    int getResult() { return result; }
+};
+
+class Add : public Operator{ 
+    // write
+};
+
+class Subtract : public Operator{ 
+    // write
+};
+
+class Multiply : public Operator{ 
+    // write
+};
+
+class Divide : public Operator{ 
+    // write
+};
+
+
+int main()
+{
+    Add a;
+    Subtract s;
+    Multiply m;
+    Divide d;
+    
+    string input_exp
+    int num1, num2;
+    char sign;
+    
+    while (1) {
+        cout << "수식을 입력하세요. (break 입력 시 종료)" << endl;
+        cin >> input_exp;
+        
+        // write
+    }
+
+    return 0;
+}
+
